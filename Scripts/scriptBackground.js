a3ec95796f604022ca9d75c3768f096401df0c41
DenisFovas <monser1001@gmail.com>
2015-10-08 20:47:17 +0300

    Add

--- Scripts/scriptBackground.js
+++ Scripts/scriptBackground.js
@@ -17,9 +17,9 @@
 		function addStars(numar){
 			for (var i = 0; i < numar; i++) {
 				game.stars.push({
-					x: Math.floor(Math.random() * 400),
-					y: 520,
-					size: Math.random() * 5
+					x: Math.floor(Math.random() * 390),
+					y: 500,
+					size: Math.random() * 3
 				});
 			};
 		}
@@ -29,11 +29,13 @@
 			addStars(1);
 			for (var i = 0; i < game.stars.length; i++) {
 				game.stars[i].y--;
-				if (game.stars[i].y < -10) {
+				if (game.stars[i].y < -5) {
 					game.stars.splice(i, 1); // functie care v-a sterge din vectorul cu stele tot ce 
+					// vector.splice(pozDePeCareStergem, nrElementeSterse)
 					// numai se afiseaza pe ecran
 				};
-				//console.log(game.stars.length); // pentru a afisa lungimea vectorilor de stele
+				console.log(game.stars.length);
+				// pentru a afisa lungimea vectorilor de stele
 			};
 		}
 
@@ -59,20 +61,21 @@
 
 		// Functie care porneste animatiile
 		function fundalActiv(){
-			animeazaFundal();
-			for (var i = 0; i < 900; i++) {
+			for (var i = 0; i < 100; i++) {
 				game.stars.push({
 					x: Math.floor(Math.random() * 400),
 					y: Math.floor(Math.random() * 600),
-					size: Math.random() * 5
+					size: Math.random() * 2
 				});
 			};
+			showStars();
+			animeazaFundal();
 		}
 
 		// Pornesc animatiile
 		fundalActiv();
 	});	
-}) ();
+})();
 
 //functie de aratat animatiile pe 93% din browsere
 window.requestAnimFrame = (function(){
@@ -83,6 +86,6 @@ window.requestAnimFrame = (function(){
           window.msRequestAnimationFrame     ||
           function( callback ){
           	// 1000 milisecunde = 1 secunda; "/ x" <=> xfps
-            window.setTimeout(callback, 1000 / 60); 
+            window.setTimeout(callback, 1000 / 20); 
           };
 })();
